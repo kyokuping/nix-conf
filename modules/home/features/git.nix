a5e7525cf87f275c3ffbd885c91539cbd9df3e0d
kyokuping <me@kyoku.dev>
2026-08-18 19:18:25 +0900

    feat(home): expand git configuration

--- modules/home/features/git.nix
+++ modules/home/features/git.nix
@@ -1,19 +1,41 @@
 { pkgs, ... }: {
   programs.git = {
+    enable = true;
+    lfs.enable = true;
+    signing.format = null;
+
     settings = {
       user = {
         name = "kyokuping";
         email = "me@kyoku.dev";
       };
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      credential.helper = "cache";
-      color.ui = true;
-      core.editor = "vim";
+      init = {
+        defaultBranch = "main";
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+      };
+      pull = {
+        rebase = true;
+      };
+      branch = {
+        sort = "-committerdate";
+      };
+      credential = {
+        helper = "cache";
+      };
+      color = {
+        ui = true;
+      };
+      core = {
+        editor = "vim";
+      };
+      alias = {
+        git = "!exec git";
+      };
     };
-    enable = true;
-    signing.format = null;
-    lfs.enable = true;
   };
+
   home.packages = [ pkgs.git-lfs ];
 }
