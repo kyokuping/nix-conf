d5f66f64177bb990fe2f5c89c274ffdf9e1b0f8c
kyokuping <me@kyoku.dev>
2026-08-27 16:43:57 +0900

    chore: update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "kyokuping's flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
